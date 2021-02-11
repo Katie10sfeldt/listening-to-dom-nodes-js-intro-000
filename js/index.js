dc1186f74d4655bddd716d5765a554a20ba0ce85
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-11 00:19:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@
 
 const main = document.getElementById('main');
 
-main.addEventListener('click', function(event){
+main.addEventListener('click', function(event) {
   alert('I was clicked!');
-})
+});
